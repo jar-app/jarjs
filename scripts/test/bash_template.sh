867dced52b2153dfd70b6a886b67c43e26499c3a
foklepoint <live.mellon@gmail.com>
2016-06-23 23:04:52 -0700

    Trying things

--- scripts/test/bash_template.sh
+++ scripts/test/bash_template.sh
@@ -2,4 +2,4 @@
 set -o errexit  # exit on command errors
 set -o pipefail # capture pipe command failures
 set -o nounset  # exit on use of unset variables
-set -o xtrace   # print command before printing
+set -o verbose   # print command before printing
--- scripts/test/test.sh
+++ scripts/test/test.sh
@@ -17,7 +17,9 @@ then
 elif [ $TEST_TYPE = "other" ]
 then
   # Find all files with the .ts extension and run tslint on them
-  docker exec --tty $CONTAINER_NAME find . -name "*.ts" -not -path "./node_modules/*" -a -not -path "./typings/*" -prune -print0 | xargs tslint
+  TS_FILES=`docker exec --tty $CONTAINER_NAME find . -name "*.ts" -not -path "./node_modules/*" -not -path "./typings/*" -prune -print0`
+  echo "Typescript Files are: $TS_FILES"
+  docker exec --tty $CONTAINER_NAME tslint $TS_FILES
   docker exec --tty jarjs_test webpack --config webpack.config.js
 elif [ $TEST_TYPE = "coverage" ]
 then
