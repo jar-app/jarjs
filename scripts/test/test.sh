cddc8c51a6ab81d1f94e602d48b3288f59e94407
foklepoint <live.mellon@gmail.com>
2016-06-23 22:25:35 -0700

    Update travis template

--- scripts/test/test.sh
+++ scripts/test/test.sh
@@ -8,16 +8,17 @@ export CONTAINER_NAME=jarjs_test
 docker run --tty --detach --name $CONTAINER_NAME jarjs:test /bin/bash
 
 # Compile ALL typescript files
-docker exec $CONTAINER_NAME tsc
+docker exec --tty $CONTAINER_NAME tsc
 
 
 if [ $TEST_TYPE = "unit" ]
 then
   echo "TODO"
-elif [ $TEST_TYPE = "lint" ]
+elif [ $TEST_TYPE = "other" ]
 then
   # Find all files with the .ts extension and run tslint on them
-  docker exec $CONTAINER_NAME find . -name "*.ts" -not \( -path "./node_modules/*" -o -path "./typings/*" \) -prune | xargs tslint
+  docker exec --tty $CONTAINER_NAME find . -name "*.ts" -not \( -path "./node_modules/*" -o -path "./typings/*" \) -prune | xargs tslint
+  docker exec --tty jarjs_test webpack --config webpack.config.js
 elif [ $TEST_TYPE = "coverage" ]
 then
   echo "TODO"
